56dd9acffbb6c275f63d6c53fdd3e1766515738c
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-07-26 20:02:18 -0400

    tela icq

--- scr/pages/ICQScreen/index.js
+++ scr/pages/ICQScreen/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { 
   Container, 
   Title, 
@@ -9,21 +10,37 @@ import {
 } from "./style";
 
 export default function ICQScreen() {
+
+  // hooks para armazenar os valores calculados
+  const [cintura, setCintura] = useState('');
+  const [quadril, setQuadril] = useState('');
+  const [icq, setIcq] = useState('')
+
+  // função para calcular o ICQ
+  function calcularIcq() {
+    let c = Number(cintura);
+    let q = Number(quadril);
+
+    let icq = c / q;
+
+    setIcq(icq.toFixed(2));
+  }
+
   return (
     <Container>
       <Title>ICQ</Title>
 
       <TextInput>Informe a cintura em cm</TextInput>
-      <Input />
+      <Input onChangeText={(v) => setCintura(v)} />
 
       <TextInput>Informe o quadril em cm</TextInput>
-      <Input />
+      <Input onChangeText={(v) => setQuadril(v)} />
 
-      <Button>
+      <Button onPress={calcularIcq}>
         <TextButton>Calcular</TextButton>
       </Button>
 
-      <TextResult>ICQ: </TextResult>
+      <TextResult>ICQ: {icq}</TextResult>
     </Container>
   )
 }
